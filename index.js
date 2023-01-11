4574a96c3feb3256b935ced89aba52908e91b654
susanahmed <susanahmed617@gmail.com>
2023-01-11 16:31:00 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 12
+const num4 = 3
+const mod = num3 / num4
+
+const max = Math.max(1, 5, 10, 15, 20);
